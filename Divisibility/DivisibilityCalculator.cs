f201a73597a5645e40e8998c6b61696fb06c10f8
heather dougherty <heatherdougherty1234@me.com>
2020-12-20 23:23:24 -0500

    simplified CheckDivisibility method

--- Divisibility/DivisibilityCalculator.cs
+++ Divisibility/DivisibilityCalculator.cs
@@ -6,20 +6,7 @@
     /// Returns true if a is evenly divisible by both b and c, otherwise return false.
     /// If either of the divisors are zero, returns false.
     /// </summary>
-    public bool CheckDivisibility(int a, int b, int c)
-    {
-      if (b == 0 || c == 0)
-      {
-        return false;
-      }
-      if (a % b == 0 && a % c == 0)
-      {
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-    }
+    public bool CheckDivisibility(int a, int b, int c) =>
+     (b != 0 && c != 0) && a % b == 0 && a % c == 0;
   }
 }
