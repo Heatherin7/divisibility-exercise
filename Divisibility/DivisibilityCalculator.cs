b0a701e038c84ca7c4bf005cf1802fb6ff43d80b
heather dougherty <heatherdougherty1234@me.com>
2020-12-20 23:09:14 -0500

    implemented divisibility calculator

--- Divisibility/DivisibilityCalculator.cs
+++ Divisibility/DivisibilityCalculator.cs
@@ -1,16 +1,25 @@
-﻿using System;
-
-namespace Divisibility
+﻿namespace Divisibility
 {
-	public class DivisibilityCalculator
-	{
-		/// <summary>
-		/// Returns true if a is evenly divisible by both b and c, otherwise return false.
-		/// If either of the divisors are zero, returns false.
-		/// </summary>
-		public bool CheckDivisibility(int a, int b, int c)
-		{
-			throw new NotImplementedException();
-		}
-	}
+  public class DivisibilityCalculator
+  {
+    /// <summary>
+    /// Returns true if a is evenly divisible by both b and c, otherwise return false.
+    /// If either of the divisors are zero, returns false.
+    /// </summary>
+    public bool CheckDivisibility(int a, int b, int c)
+    {
+      if (b == 0 || c == 0)
+      {
+        return false;
+      }
+      if (a % b == 0 && a % c == 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
 }
